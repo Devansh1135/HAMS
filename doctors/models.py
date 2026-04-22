46d68af506756d25d6317a7b6808348c41025d1d
Devansh <devansh.jagatiya@simformsolutions.com>
2026-04-22 19:02:16 +0530

    added doctor app and models

---
+++ doctors/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from accounts.models import User
+# Create your models here.
+
+class DoctorProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    firstname = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20)
+    sex = models.CharField(choices=[("M", "male"), ("F", "female")], max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    specialty = models.CharField(max_length=20)
+    degree = models.CharField(max_length=10)
+    
+class DoctorAvailibility(models.Model):
+    days = [
+            (0, 'Monday'),
+            (1, 'Tuesday'),
+            (2, 'Wednesday'),
+            (3, 'Thursday'),
+            (4, 'Friday'),
+            (5, 'Saturday'),
+            (6, 'Sunday'),
+        ]
+    doctor = models.ForeignKey(DoctorProfile, on_delete=models.CASCADE)
+    days_of_week = models.IntegerField(choices = days)
+    time_in = models.TimeField()
+    time_out = models.TimeField()
+    slot_duration = models.IntegerField()
+
